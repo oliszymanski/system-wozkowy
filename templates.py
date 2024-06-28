bbe09f1762fa19faa1a3c224e5f1cfaa0d796680
oliwier.szymanski112 <oliwier.szymanski112@gmail.com>
2024-06-28 05:40:09 +0000

    modify: adding templates for every day; adding header row

---
+++ templates.py
@@ -0,0 +1,54 @@
+header = '''
+godzina dyżuru,głosiciel 1,głosiciel 2,zastępstwo 1,zastępstwo 2
+'''
+day_wtorek = '''
+O.K i S.
+7:00,-,-,-,-
+8:00,-,-,-,-
+
+Park od strony mostku
+7:00,-,-,-,-
+8:00,-,-,-,-
+'''
+
+day_czwartek = '''
+O.K i S.
+7:00,-,-,-,-
+8:00,-,-,-,-
+
+Bazarek od strony parku - przy zejściu na perony
+12:00,-,-,-,-
+13:00,-,-,-,-
+14:00,-,-,-,-
+15:00,-,-,-,-
+16:00,-,-,-,-
+'''
+
+day_piatek = '''
+Park od strony mostku
+7:00,-,-,-,-
+8:00,-,-,-,-
+
+Kolejowa API
+17:00,-,-,-,-
+18:30,-,-,-,-
+'''
+
+day_sobota = '''
+Park od strony bazarku
+7:30,-,-,-,-
+8:30,-,-,-,-
+9:30,-,-,-,-
+10:30,-,-,-,-
+11:30,-,-,-,-
+12:30,-,-,-,-
+13:30,-,-,-,-
+
+Skwer na przeciwko żabki (koło tunelu)
+7:30,-,-,-,-
+8:30,-,-,-,-
+
+Kolejowa API
+12:00,-,-,-,-
+13:00,-,-,-,-
+'''
