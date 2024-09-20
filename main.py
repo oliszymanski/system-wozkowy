4b779c042f7e1e49e0e43598d6eee2352997ba98
oliszymanski <oliwier.szymanski112@gmail.com>
2024-09-20 07:56:21 +0000

    ..

--- main.py
+++ main.py
@@ -7,33 +7,6 @@ import assign
 
 
 #=======================================================
-#	GLOBALS:
-#=======================================================
-
-_DBG0_ = False                               # debuggers
-_DBG1_ = True
-
-month = 9                                   # calendar setup
-year = 2024
-
-dict_days = {
-    'wtorek':'Tuesday',
-    'czwartek':'Thursday',
-    'piątek':'Friday',
-    'sobota':'Saturday'
-}
-
-ls_fieldnames = [ 'godzina dyżuru', 'głosiciel 1', 'głosiciel 2' ]
-
-ls_weekdays = list( dict_days.keys() )
-
-csv_grafik_reader = csv.DictReader( open('grafik.csv', 'r') )
-csv_grafik_writer = csv.DictWriter( open('grafik.csv', 'r'), fieldnames=ls_fieldnames )
-csv_dyspozycyjnosc_reader = csv.DictReader( open('dyspozycyjnosc.csv', 'r') )
-
-
-
-#=======================================================
 #	FUNCTIONS:
 #=======================================================
 
@@ -45,7 +18,6 @@ csv_dyspozycyjnosc_reader = csv.DictReader( open('dyspozycyjnosc.csv', 'r') )
 #=======================================================
 
 if ( __name__ == '__main__' ):
-    assign_people( 'sobota', 202, 'grafik.csv', 'dyspozycyjnosc.csv' )
 
 
 
